091050d48380e6d30b667102ac01d7d49fbec260
bheemaguli <sumanth.bhi@gmail.com>
2022-06-27 21:49:44 +0530

    Set Logger and Exception

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
 from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +10,12 @@ bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    try:
+        raise Exception('Testing exception module...')
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.exception('Testing exception module...')
+        logging.error(housing.error_message)
     logging.info('Initial Log.')
     return render_template('index.html')
 

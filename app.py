62135ae325a2fc468a70b7ce62a16fbc00ed11f2
bheemaguli <sumanth.bhi@gmail.com>
2022-07-26 10:57:35 +0530

    Added log file viewing component to the web page

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_bootstrap import Bootstrap5
 from housing.logger import logging
 from housing.exception import HousingException
+from housing.config.config import Configuartion
+from housing.constant import CONFIG_DIR, get_current_time_stamp
+from housing.pipeline.pipeline import Pipeline
+from housing.entity.housing_predictor import HousingPredictor, HousingData
+from housing.util.util import read_yaml_file, write_yaml_file
+
+from flask import Flask, render_template, url_for, redirect, request, \
+    send_file, abort
+from flask_bootstrap import Bootstrap5
+from matplotlib.style import context
+import os, sys
+import json
 import sys
+
+ROOT_DIR = os.getcwd()
+LOG_FOLDER_NAME = "logs"
+PIPELINE_FOLDER_NAME = "housing"
+SAVED_MODELS_DIR_NAME = "saved_models"
+MODEL_CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, "model.yaml")
+LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
+PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
+MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
+HOUSING_DATA_KEY = "housing_data"
+MEDIAN_HOUSING_VALUE_KEY = "median_house_value"
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -10,7 +31,10 @@ bootstrap = Bootstrap5(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
 
 @app.route('/roadmap', methods=['GET'])
 def roadmap():
@@ -32,6 +56,159 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html', e=e)
 
+@app.route('/artifact', defaults={'req_path': 'housing'})
+@app.route('/artifact/<path:req_path>')
+def render_artifact_dir(req_path):
+    os.makedirs("housing", exist_ok=True)
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    if os.path.isfile(abs_path):
+        if ".html" in abs_path:
+            with open(abs_path, "r", encoding="utf-8") as file:
+                content = ''
+                for line in file.readlines():
+                    content = f"{content}{line}"
+                return content
+        return send_file(abs_path)
+
+    files = {os.path.join(abs_path, file_name): file_name for file_name in os.listdir(abs_path) if
+             "artifact" in os.path.join(abs_path, file_name)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('files.html', result=result)
+
+@app.route('/data/view_experiment_hist', methods=['GET', 'POST'])
+def view_experiment_history():
+    experiment_df = Pipeline.get_experiments_status()
+    context = {
+        "experiment": experiment_df.to_html(classes='table table-striped col-12')
+    }
+    return render_template('experiment_history.html', context=context)
+
+
+@app.route('/data/train', methods=['GET', 'POST'])
+def train():
+    message = ""
+    pipeline = Pipeline(config=Configuartion(current_time_stamp=get_current_time_stamp()))
+    if not Pipeline.experiment.running_status:
+        message = "Training started."
+        pipeline.start()
+    else:
+        message = "Training is already in progress."
+    context = {
+        "experiment": pipeline.get_experiments_status().to_html(classes='table table-striped col-12'),
+        "message": message
+    }
+    return render_template('train.html', context=context)
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    context = {
+        HOUSING_DATA_KEY: None,
+        MEDIAN_HOUSING_VALUE_KEY: None
+    }
+
+    if request.method == 'POST':
+        longitude = float(request.form['longitude'])
+        latitude = float(request.form['latitude'])
+        housing_median_age = float(request.form['housing_median_age'])
+        total_rooms = float(request.form['total_rooms'])
+        total_bedrooms = float(request.form['total_bedrooms'])
+        population = float(request.form['population'])
+        households = float(request.form['households'])
+        median_income = float(request.form['median_income'])
+        ocean_proximity = request.form['ocean_proximity']
+
+        housing_data = HousingData(longitude=longitude,
+                                   latitude=latitude,
+                                   housing_median_age=housing_median_age,
+                                   total_rooms=total_rooms,
+                                   total_bedrooms=total_bedrooms,
+                                   population=population,
+                                   households=households,
+                                   median_income=median_income,
+                                   ocean_proximity=ocean_proximity,
+                                   )
+        housing_df = housing_data.get_housing_input_data_frame()
+        housing_predictor = HousingPredictor(model_dir=MODEL_DIR)
+        median_housing_value = housing_predictor.predict(X=housing_df)
+        context = {
+            HOUSING_DATA_KEY: housing_data.get_housing_data_as_dict(),
+            MEDIAN_HOUSING_VALUE_KEY: median_housing_value,
+        }
+        return render_template('predict.html', context=context)
+    return render_template("predict.html", context=context)
+
+
+@app.route('/data/saved_models', defaults={'req_path': 'saved_models'})
+@app.route('/data/saved_models/<path:req_path>')
+def saved_models_dir(req_path):
+    os.makedirs("saved_models", exist_ok=True)
+    print(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('saved_models_files.html', result=result)
+
+
+@app.route("/data/update_model_config", methods=['GET', 'POST'])
+def update_model_config():
+    try:
+        if request.method == 'POST':
+            model_config = request.form['new_model_config']
+            model_config = model_config.replace("'", '"')
+            print(model_config)
+            model_config = json.loads(model_config)
+
+            write_yaml_file(file_path=MODEL_CONFIG_FILE_PATH, data=model_config)
+
+        model_config = read_yaml_file(file_path=MODEL_CONFIG_FILE_PATH)
+        return render_template('update_model.html', result={"model_config": model_config})
+
+    except  Exception as e:
+        logging.exception(e)
+        return str(e)
+
+
+@app.route(f'/data/logs', defaults={'req_path': f'{LOG_FOLDER_NAME}'})
+@app.route(f'/{LOG_FOLDER_NAME}/<path:req_path>')
+def render_log_dir(req_path):
+    os.makedirs(LOG_FOLDER_NAME, exist_ok=True)
+    logging.info(f"req_path: {req_path}")
+    abs_path = os.path.join(req_path)
+    print(abs_path)
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    files = {os.path.join(abs_path, file): file for file in os.listdir(abs_path)}
+
+    result = {
+        "files": files,
+        "parent_folder": os.path.dirname(abs_path),
+        "parent_label": abs_path
+    }
+    return render_template('logs.html', result=result)
     
 if __name__ == '__main__':
     app.run()
